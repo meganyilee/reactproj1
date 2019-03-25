b9d748bf9a5c4f32382b8f881ad55cd87a73950a
Megan Lee <meganylee@berkeley.edu>
2019-03-24 20:51:15 -0700

    finished p1

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,31 @@
 import React from "react";
+import Product from "./Product.js";
 import "./styles/cart.css";
+import ProductData from "./Data"
 
 class Cart extends React.Component {
 
   render() {
     return (
         <div className="page-content">
-            <h2>Add your products here!</h2>
+            <h2>
+              <Product 
+                productName = "Apple"
+                price = "1"
+                limit = {100}/>
+              <Product 
+                productName = "Banana"
+                price = {0.5}
+                limit = {20}/>
+              <Product 
+                productName = "Orange"
+                price = {2}
+                limit = {5}/>
+              <Product 
+                productName = "Durian"
+                price = {5}
+                limit = {0}/>
+            </h2>
         </div>
     );
   }
